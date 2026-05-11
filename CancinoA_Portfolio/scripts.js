60986d699ef81aed006572de356c101d4a6cc876
Adriana Cancino <243426989+CrazyArtist713@users.noreply.github.com>
2026-05-11 08:35:42 -0500

    Complete Portfolio
    You can click on the gallery sections to view the sseparate galleries

--- CancinoA_Portfolio/scripts.js
+++ CancinoA_Portfolio/scripts.js
@@ -4,3 +4,22 @@ function toggleNav() {
      elements[i].classList.toggle('show');
      }
 }
+document.addEventListener("DOMContentLoaded", function() {
+    // Photography
+    document.querySelector('.photography>.block').addEventListener('click', function(){
+        document.querySelector('.photography>.images').classList.toggle('show');
+        document.querySelector('.photography span').classList.toggle('rotate');
+    });
+
+    // Traditional
+    document.querySelector('.traditional>.block').addEventListener('click', function(){
+        document.querySelector('.traditional>.images').classList.toggle('show');
+        document.querySelector('.traditional span').classList.toggle('rotate');
+    });
+
+    // Digital
+    document.querySelector('.digital>.block').addEventListener('click', function(){
+        document.querySelector('.digital>.images').classList.toggle('show');
+        document.querySelector('.digital span').classList.toggle('rotate');
+    });
+});
